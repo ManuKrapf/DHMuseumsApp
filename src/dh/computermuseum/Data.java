f1c20d05ca9f2e4719217eb494d6696d12ce3a9d
Manuel Krapf <manuel@krapfnet.de>
2014-03-06 15:35:53 +0100

    tracking works

---
+++ src/dh/computermuseum/Data.java
@@ -0,0 +1,69 @@
+package dh.computermuseum;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.util.Log;
+
+public class Data {
+	
+	Context context; 
+	
+	private List<Antique> antiques;
+	private AntiquesContentHandler ach;
+	
+	public Data(Context c) {
+		context = c;
+		initXMLParser();
+	}
+	
+	private void initXMLParser() {
+		try {
+			SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+			SAXParser newSAXParser = saxParserFactory.newSAXParser();
+			XMLReader xmlReader = newSAXParser.getXMLReader();
+			// read XML input file here:
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					context.getAssets().open("museum.xml")));
+			InputSource inputSource = new InputSource(br);
+			ach = new AntiquesContentHandler();
+			xmlReader.setContentHandler(ach);
+			xmlReader.parse(inputSource);
+			
+			antiques = ach.getAntiques();
+			Log.d("XML", ach.getAntiques().toString());
+
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (SAXException saxe) {
+			saxe.printStackTrace();
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}
+	}
+	
+	public Antique getAntique(String name) {
+		for(Antique a : antiques) {
+			if(a.getName().equals(name)) {
+				return a;
+			}
+		}
+		return null;
+	}
+
+}
--- src/dh/computermuseum/ScanActivity.java
+++ src/dh/computermuseum/ScanActivity.java
@@ -27,110 +27,30 @@ import com.metaio.sdk.jni.Vector3d;
 import com.metaio.tools.io.AssetsManager;
 
 public class ScanActivity extends ARViewActivity {
-	SurfaceView surfaceView;
-	SurfaceHolder surfaceHolder;
-	Camera camera;
-	private boolean inPreviewMode = false;
 	
 	Context context = this;
+	Data data;
+	TextView text;
 	
 	private IGeometry mModel;
+	private IGeometry mModel2;
 	
 	private MetaioSDKCallbackHandler mCallbackHandler;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.scan);
-		//initUI();
-		//setupClickListener();
+		
+		data = new Data(this);
 		mCallbackHandler = new MetaioSDKCallbackHandler();
 	}
-	/*
-	private void initUI() {
-		initCameraPreview();
-	}
-	
-	private void setupClickListener() {
-		TextView scan = (TextView) findViewById(R.id.shortScan);
-		scan.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mModel.setVisible(true);
-				mCallbackHandler.onTrackingEvent(metaioSDK.getTrackingValues());
-			}
-		});
-		//surfaceView.setOnClickListener(this);
-	}
-	
-	
-	private void initCameraPreview() {
-		surfaceView = (SurfaceView) findViewById(R.id.cameraPreview);
-		surfaceHolder = surfaceView.getHolder();
-		surfaceHolder.addCallback(surfaceCallback);
-		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
-	
-	
-	
-	public void onClick(View view) {
-    	switch (view.getId()) {
-			case  R.id.cameraPreview:
-				Intent i = new Intent(ScanActivity.this, DetailActivity.class);
-			    startActivity(i);
-				break;
-			default:
-				break;
-		}
-    }*/
 
 	public void onResume() {
 		super.onResume();
-		/*camera = Camera.open();
-		camera.setDisplayOrientation(90);
-		startCameraPreview();*/
 	}
 
 	public void onPause() {
-		/*if (inPreviewMode) {
-			camera.stopPreview();
-		}
-		camera.release();
-		camera = null;
-		inPreviewMode = false;*/
 		super.onPause();
 	}
-	/*
-	private void displayCameraPreview(int width, int height) {
-		if (camera != null && surfaceHolder.getSurface() != null) {
-			try {
-				camera.setPreviewDisplay(surfaceHolder);
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
-			}
-		}
-	}
-
-	private void startCameraPreview() {
-		if (camera != null) {
-			camera.startPreview();
-			inPreviewMode = true;
-		}
-	}
-
-	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-		public void surfaceCreated(SurfaceHolder holder) {
-		}
-
-		public void surfaceChanged(SurfaceHolder holder, int format, int width,
-				int height) {
-			displayCameraPreview(width, height);
-			startCameraPreview();
-		}
-
-		public void surfaceDestroyed(SurfaceHolder holder) {
-		}
-	};*/
 
 	@Override
 	protected int getGUILayout() {
@@ -148,14 +68,17 @@ public class ScanActivity extends ARViewActivity {
 		{
 			AssetsManager.extractAllAssets(getApplicationContext(), BuildConfig.DEBUG);
 			// Getting a file path for tracking configuration XML file
-			String trackingConfigFile = AssetsManager.getAssetPath("3dmap/desktop.zip");
+			String trackingConfigFile = AssetsManager.getAssetPath("3dmap/3dmaps.zip");
 			
 			//Log.d("DEBUG", trackingConfigFile);
 			
 			// Assigning tracking configuration
 			boolean result = metaioSDK.setTrackingConfiguration(trackingConfigFile); 
-			MetaioDebug.log("Tracking data loaded: " + result); 
-	        
+			MetaioDebug.log("Tracking data loaded: " + result);
+			
+			//loadModel();
+			
+	        /*
 			// Getting a file path for a 3D geometry
 			String metaioManModel = AssetsManager.getAssetPath("Assets/metaioman.md2");			
 			if (metaioManModel != null)
@@ -171,12 +94,13 @@ public class ScanActivity extends ARViewActivity {
 					mModel.setScale(new Vector3d(4.0f, 4.0f, 4.0f));
 					
 				}
-				else
+				else {
 					Log.d("METAIO", "METAIOMAN ist eine null!");
 					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+metaioManModel);
-			}
+				}
+			}*/
 			
-			mModel.setVisible(true);
+			//mModel.setVisible(true);
 			//mCallbackHandler.onTrackingEvent(metaioSDK.getTrackingValues());
 		}
 		catch (Exception e)
@@ -193,9 +117,11 @@ public class ScanActivity extends ARViewActivity {
 	
 	final class MetaioSDKCallbackHandler extends IMetaioSDKCallback 
 	{
-
+		
+		
+		
 		@Override
-		public void onSDKReady() 
+		public void onSDKReady()
 		{
 			// show GUI
 			runOnUiThread(new Runnable() 
@@ -204,30 +130,174 @@ public class ScanActivity extends ARViewActivity {
 				public void run() 
 				{
 					mGUIView.setVisibility(View.VISIBLE);
+					text = (TextView) findViewById(R.id.timeline);
 				}
 			});
 		}
-		/*
+		
 		@Override
 		public void onTrackingEvent(TrackingValuesVector values) {
 			
 			super.onTrackingEvent(values);
 			
-			TrackingValues tv = values.get(0);
+			loadModel();
+			
+			//TrackingValues tv = values.get(0);
 			
 			//ETRACKING_STATE state = ETRACKING_STATE.ETS_FOUND;
 			
+			if (values.size() > 0) {
+				
+				String temp = "";
+				
+				if(values.get(0).getState() == ETRACKING_STATE.ETS_FOUND) {
+					Log.d("dhdebug", values.get(0).getCosName()+" is found");
+					
+					if(values.get(0).getCosName().equals("watch_1")) {
+						Log.d("dhdebug", "ID watch: "+values.get(0).getCoordinateSystemID());
+						temp = getDataFromXML(1);
+					}
+					else if(values.get(0).getCosName().equals("keyboard_2")) {
+						Log.d("dhdebug", "ID tastatur: "+values.get(0).getCoordinateSystemID());
+						temp = getDataFromXML(2);
+					}
+					else if(values.get(0).getState() == ETRACKING_STATE.ETS_LOST) {
+						//text.setVisibility(View.INVISIBLE);
+					}
+					
+					final String textval = temp;
+					
+					runOnUiThread(new Runnable() 
+					{
+						@Override
+						public void run() 
+						{
+							text.setText(textval);
+						}
+					});
+					
+					//loadModel();
+				}
+				else {
+					Log.d("dhdebug", "nothing is registered or found");
+				}
+				
+				Log.d("dhdebug", "Values: "+values.size());
+				Log.d("dhdebug", "Name 0: "+values.get(0).getCosName());
+				Log.d("dhdebug", "State 0: "+values.get(0).getState());
+				if(values.size() >= 2) {
+					Log.d("dhdebug", "Name 1: "+values.get(1).getCosName());
+					Log.d("dhdebug", "State 1: "+values.get(1).getState());
+				}
+				
+			}
+			
+			/*
 			if(tv.isTrackingState(ETRACKING_STATE.ETS_FOUND)) {
 				mModel.setVisible(true);
 			}
+			
 			
 			CharSequence text = "Tracking Event fired!";
 			int duration = Toast.LENGTH_SHORT;
 
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
+			*/
+		}
+	}
+	
+	private void loadModel() {
+		
+		try
+		{
+	        
+			if(mModel != null) {
+				metaioSDK.unloadGeometry(mModel);
+			}
 			
-		}*/
+			if(mModel2 != null) {
+				metaioSDK.unloadGeometry(mModel2);
+			}
+			
+			// Getting a file path for a 3D geometry
+			String metaioManModel = AssetsManager.getAssetPath("Assets/metaioman.md2");			
+			if (metaioManModel != null)
+			{
+				Log.d("METAIO", "File 1 found!!!");
+				
+				// Loading 3D geometry
+				mModel = metaioSDK.createGeometry(metaioManModel);
+				if (mModel != null) 
+				{
+					Log.d("METAIO", "Model 1 nicht null");
+					// Set geometry properties
+					mModel.setScale(new Vector3d(4.0f, 4.0f, 4.0f));
+					mModel.setCoordinateSystemID(1);
+					
+				}
+				else {
+					Log.d("METAIO", "METAIOMAN ist eine null!");
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+metaioManModel);
+				}
+			}
+			
+			String otherModel = AssetsManager.getAssetPath("arel2/bild.jpg");			
+			if (otherModel != null)
+			{
+				Log.d("METAIO", "File 2 found!!!");
+				
+				
+				
+				// Loading 3D geometry
+				mModel2 = metaioSDK.createGeometryFromImage(otherModel);
+				if (mModel2 != null) 
+				{
+					Log.d("METAIO", "Model 2 nicht null");
+					// Set geometry properties
+					//mModel.setScale(new Vector3d(4.0f, 4.0f, 4.0f));
+					mModel2.setCoordinateSystemID(2);
+					
+				}
+				else {
+					Log.d("METAIO", "METAIOMAN ist eine null!");
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+otherModel);
+				}
+			}
+			
+			//mModel.setVisible(true);
+			//mCallbackHandler.onTrackingEvent(metaioSDK.getTrackingValues());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private String getDataFromXML(int id) {
+		
+		String name = "";
+		
+		switch(id) {
+			case 1: name = "Yost No. 15";
+				break;
+			case 2: name = "Toshiba T3200SX";
+				break;
+			case 3: 
+				break;
+			case 4: 
+				break;
+			default: name = "Megacomputer";
+				break;
+		}
+		
+		Antique ant = data.getAntique(name);
+		Log.d("dhdebug", "Antique: ID: "+ant.getId()+", Name: "+ant.getName()+", Date: "+ant.getReleaseDate());
+		
+		return "Antique: ID: "+ant.getId()+", Name: "+ant.getName()+", Date: "+ant.getReleaseDate();
+		
+		//text.setVisibility(View.VISIBLE);
 	}
 	
 }
