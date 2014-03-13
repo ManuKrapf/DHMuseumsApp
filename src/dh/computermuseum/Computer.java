10f3ccdadf9c4100bfa8a2e3dae981add8398388
SimonProv <simon@provinsky.de>
2014-03-13 13:09:45 +0100

    Update Computer.java

--- src/dh/computermuseum/Computer.java
+++ src/dh/computermuseum/Computer.java
@@ -1,5 +1,71 @@
 package dh.computermuseum;
 
 public class Computer extends Antique {
-
+  private String os;
+  private String memory;
+  private String ram;
+  private String cpu;
+  private String special;
+  private String img;
+  private String description;
+  
+  public Computer() {
+    
+  }
+  
+  public void setOs(String os) {
+		this.os = os;
+	}
+	
+	public String getOs() {
+		return os;
+	}
+	
+	public void setMemory(String memory) {
+		this.memory = memory;
+	}
+	
+	public String getMemory() {
+		return memory;
+	}
+	
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	
+	public String getRam() {
+		return ram;
+	}
+	
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+	
+	public String getCpu() {
+		return cpu;
+	}
+	
+	public void setSpecial(String special) {
+		this.special = special;
+	}
+	
+	public String getSpecial() {
+		return special;
+	}
+	
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	public String getImg() {
+		return img;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
 }
