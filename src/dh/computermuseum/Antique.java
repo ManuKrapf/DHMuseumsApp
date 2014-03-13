bf27b708b8adfcd3b0437d482a53dec812e6481c
SimonProv <simon@provinsky.de>
2014-03-13 13:21:32 +0100

    Update Antique.java

--- src/dh/computermuseum/Antique.java
+++ src/dh/computermuseum/Antique.java
@@ -3,15 +3,7 @@ package dh.computermuseum;
 public class Antique {
 	private String name;
 	private int id;
-	private String desc;
 	private String release;
-	private String producer;
-	private String type;
-	private String os;
-	private String memory;
-	private String ram;
-	private String cpu;
-	private String special;
 	private String img;
 
 	public Antique() {
@@ -33,14 +25,6 @@ public class Antique {
 		return id;
 	}
 
-	public void setDescription(String desc) {
-		this.desc = desc;
-	}
-
-	public String getDescription() {
-		return desc;
-	}
-
 	public void setReleaseDate(String release) {
 		this.release = release;
 	}
@@ -49,62 +33,6 @@ public class Antique {
 		return release;
 	}
 
-	public void setProducer(String producer) {
-		this.producer = producer;
-	}
-
-	public String getProducer() {
-		return producer;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	
-	public void setOs(String os) {
-		this.os = os;
-	}
-	
-	public String getOs() {
-		return os;
-	}
-	
-	public void setMemory(String memory) {
-		this.memory = memory;
-	}
-	
-	public String getMemory() {
-		return memory;
-	}
-	
-	public void setRam(String ram) {
-		this.ram = ram;
-	}
-	
-	public String getRam() {
-		return ram;
-	}
-	
-	public void setCpu(String cpu) {
-		this.cpu = cpu;
-	}
-	
-	public String getCpu() {
-		return cpu;
-	}
-	
-	public void setSpecial(String special) {
-		this.special = special;
-	}
-	
-	public String getSpecial() {
-		return special;
-	}
-	
 	public void setImg(String img) {
 		this.img = img;
 	}
@@ -112,10 +40,4 @@ public class Antique {
 	public String getImg() {
 		return img;
 	}
-
-	public String toString() {
-		return "[[" + this.id + "] [" + this.name + "] [" + this.desc + "]"
-				+ " [" + this.release + "] [" + this.producer + " ]]";
-	}
 }
-
